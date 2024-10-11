25d82d811ecbfe25107993f4b0d03d3b93ce1094
tugu <tugit8833@gmail.com>
2024-10-11 15:49:23 +0800

    medkue

--- Godot/Scripts/resources/question.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name QuizRush
-
-@export var question_info : String
-@export var type : Enum.qType
-@export var options: Array[String]
-@export var correct: String
--- Godot/enemy_components/Dead.gd
+++
@@ -1,9 +0,0 @@
-extends EnemyState
-
-func on_enter():
-	playback.travel("die")
-	$Timer.start()
-
-
-func _on_timer_timeout():
-	get_parent().get_parent().queue_free()
